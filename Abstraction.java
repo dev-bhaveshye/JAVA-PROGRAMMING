e1882378bce87432daf6280eacd44dbf9c5a94b6
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-03-03 23:26:04 +0530

    Add files via upload

---
+++ Abstraction.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Abstraction {
+    public static void main(String[] args){
+        class car{
+            int price;
+
+            void start(){
+            System.out.println("car is starting");
+        }
+        }
+
+    }
+    
+}
