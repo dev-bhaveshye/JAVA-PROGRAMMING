6b33d79a4b9d17143ab9bc4f5efed0cd8e6b251f
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2025-06-12 13:27:43 +0530

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,18 @@
+package Exercise;
+
+public class Ex1 {
+    public static void main(String[] args){
+
+    
+        //Write a Java program to get a number from the user and print whether it is positive or negative.
+        int num = 35 ;
+
+        if (num>=0){
+            System.out.println("num is positive");
+        }else {
+            System.out.println("system is negative ");
+        }
+    }
+}
+
+
