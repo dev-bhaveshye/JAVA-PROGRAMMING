742e2e5a8b252f412a481bcfe5493ded212ffbd0
Bhavesh Yelekar <Bhaveshyatwork@gmail.com>
2026-01-15 21:59:06 +0530

    Add files via upload

---
+++ pract1.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class pract1 {
+    public static void main (String[] args){
+
+
+        // reverse a array using array
+
+        int[] arr = {1,2,3,4,5,6};
+
+        int start = 0;
+        int end = arr.length-1;
+
+
+        while(start<end){
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
+
+
+    }
+}
